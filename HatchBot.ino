5a5cf9743fc73180d28984682a9cb8f5cb1a38f5
dizymac <mcgahan.dave@gmail.com>
2015-03-07 19:39:26 +1300

    Create HatchBot.ino
    First commit.

---
+++ HatchBot.ino
@@ -0,0 +1,22 @@
+/*
+*
+*
+*  HatchBot
+*  New Zealand DOC Kākāpō Recovery program
+*  David McGahan 2015
+*  @dizymac
+*  www.davidmcgahan.co.nz
+*
+*  http://en.wikipedia.org/wiki/Kakapo
+*
+*
+*
+*/
+
+void setup() {
+
+}
+
+void loop() {
+
+}
