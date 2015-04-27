de583b707336055ed856a733df2cee8dd35801e8
dizymac <mcgahan.dave@gmail.com>
2015-04-28 00:09:45 +1200

    Audio .WAV file playing now works
    WAV files now play back successfully.

--- HatchBot.ino
+++ HatchBot.ino
@@ -1,12 +1,12 @@
 /*
 *
-*  HatchBot v0.03a 12/04/2015
+*  HatchBot v0.04a 27/04/2015
 *  for the New Zealand Department of Conservation 
 *  Kākāpō Recovery program
 *  David McGahan
 *  @dizymac
 *
-*  For more information on Kākāpō please see below:
+*  For more information on Kākāpō birds please see below:
 *  http://en.wikipedia.org/wiki/Kakapo
 *
 *  This work is licensed under the Creative Commons 
@@ -16,51 +16,56 @@
 *
 *
 */
-
+#include <pcmConfig.h>
+#include <pcmRF.h>
+#include <TMRpcm.h>
+#include <SPI.h>
+#include <SD.h>
 #include <LowPower.h>
-const int speakerPin = 8;
-const int motorPin = 9;
+
+#define SD_ChipSelectPin 10
+#define motorPin 3
+
+TMRpcm chirp;
 
 void setup() {
+  chirp.speakerPin = 9;
   pinMode(motorPin, OUTPUT);
+  Serial.begin(9600);
+  if (!SD.begin(SD_ChipSelectPin)) {}
 }
 void loop() {
   int randNumber = random(8000); // Define random variable time. in this case, 10 minutes or 600000ms is required.
-  int chirpCount = random(5); // Define a random number of times to chirp.
+  int chirpCount = random(2); // Define a random number of times to chirp.
   int i = 0;
   while (i < chirpCount){
     i++;
-    chirp();
+    chirp.play("K2.WAV");
     delay(random(4000)); // Allow a random and non-uniform time between chirps.
   }
-  for (byte motorSpeed = 0; 
-    motorSpeed <= 255; 
-    motorSpeed +=5 
-    ) // DC motor PWM control 0 - 255
-    {
-      analogWrite(motorPin, motorSpeed);
-      delay(500);
-    }
+  motor();
   delay(randNumber);
-  // LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF); // Power down mode to preserve battery until next loop starts up.
+  LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF); // Power down mode to preserve battery until next loop starts up.
 }
 
-// Below is from Adafruit, to be replaced by WAV recordings of the Kakapo egg.
-void chirp() {  // Bird chirp
-  for(uint8_t i=200; i>180; i--)
-     playTone(i,9);
+void motor() {
+  byte speed = 50;
+  analogWrite(motorPin, speed);
+  byte maxSpeed = random(25, 45);
+    while (speed < maxSpeed) 
+     { 
+       analogWrite(motorPin, speed);
+       {
+         speed++;
+         for (int i=8; i>5; i--){
+           delay(1);
+         }
+       }
+     }
+     if (speed == maxSpeed) 
+     { 
+       analogWrite(motorPin, 0); 
+       //delay(random(1000, 5000)); test that this uses the timing of the randNumber variable
+     }
 }
-void playTone(uint16_t tone1, uint16_t duration) {
-  if(tone1 < 50 || tone1 > 15000) return;  // these do not play on a piezo
-  for (long i = 0; i < duration * 1000L; i += tone1 * 2) {
-     digitalWrite(speakerPin, HIGH);
-     delayMicroseconds(tone1);
-     digitalWrite(speakerPin, LOW);
-     delayMicroseconds(tone1);
-  }     
-}
-void scale() {
-  for(uint16_t i=50; i<15000; i++)  {
-     playTone(i,20);
-  }
-}
+
